aec6cb7848069f671541604e6fedf8beaf7abe2b
NonCompeteCoop <noncompetecoop@proton.me>
2025-12-18 10:29:35 +0700

    Create config.mjs

---
+++ config.mjs
@@ -0,0 +1,13 @@
+export const config = {
+  title: 'Non-Compete Collective',
+  collections: {
+    nc_videos: {
+      path: 'NonCompete/Videos',
+      showListing: true,
+    },
+    luna_videos: {
+      path: 'LunaOi/Videos',
+      showListing: true,
+    },
+  },
+}
